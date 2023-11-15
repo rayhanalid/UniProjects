1fa7e8ad7990ff3ce9a7503610decc28d583d8f3
rayhanalid <57931507+rayhanalid@users.noreply.github.com>
2023-11-15 16:31:40 +0700

    Delete PATCH-Mobile-APP-master directory

--- PATCH-Mobile-APP-master/PetDatingApp/src/main/java/com/PATCH/PetDatingApp/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-/*
-package com.PATCH.PetDatingApp.exception;
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}*/
--- PATCH-Mobile-APP-master/PetDatingApp/src/main/java/com/PATCH/PetDatingApp/service/PetImageService.java
+++
@@ -1,9 +0,0 @@
-package com.PATCH.PetDatingApp.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface PetImageService {
-    String uploadPetImage(String pid, MultipartFile file) throws IOException;
-}
